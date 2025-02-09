2445460227a7aa6e43d15ece360816cc74c8abf5
Emily <emily0606.cwo@gmail.com>
2025-02-09 15:30:28 +0800

    [feat] init

---
+++ src/main/java/com/omg/OMG/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.omg.OMG;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OmgApplication.class);
+	}
+
+}
